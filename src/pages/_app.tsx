882c025d51904df11373322ebfcdd023da6ab911
George Yin Xu <georgeyinxu@gmail.com>
2023-04-15 21:27:52 +0800

    created basic ui structure with sidebar and body for the dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { Fragment } from 'react';
+import Home from '.';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = () => {
+  return (
+    <Fragment>
+      <Home />
+    </Fragment>
+  );
+};
+
+export default App;
